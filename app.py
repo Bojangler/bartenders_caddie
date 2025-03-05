947629baaefbc491ca222bd7d80288adf96dfb5b
rossmarshall <ross.w.marshall@gmail.com>
2025-03-04 19:50:52 -0800

    Final prototype before adding cocktails.json

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# filepath: /c:/Users/rossw/Documents/bartenders_caddie/app.py
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -20,25 +21,51 @@ def suggest_cocktails(available_ingredients):
     """
     suggestions = []
     almost_suggestions = []
+    missing_ingredients = {}
+    ingredient_cocktail_count = {ingredient: 0 for ingredient in available_ingredients}
+    
     for cocktail, ingredients in cocktails.items():
-        missing_ingredients = [ingredient for ingredient in ingredients if ingredient not in available_ingredients]
-        if len(missing_ingredients) == 0:
-            suggestions.append(cocktail)
-        elif len(missing_ingredients) <= 2:
-            almost_suggestions.append((cocktail, missing_ingredients))
-    return suggestions, almost_suggestions
+        missing = [ingredient for ingredient in ingredients if ingredient not in available_ingredients]
+        if len(missing) == 0:
+            suggestions.append((cocktail, ingredients))
+            for ingredient in ingredients:
+                if ingredient in ingredient_cocktail_count:
+                    ingredient_cocktail_count[ingredient] += 1
+        elif len(missing) <= 2:
+            almost_suggestions.append((cocktail, missing))
+        for ingredient in missing:
+            if ingredient in missing_ingredients:
+                missing_ingredients[ingredient] += 1
+            else:
+                missing_ingredients[ingredient] = 1
+    return suggestions, almost_suggestions, missing_ingredients, ingredient_cocktail_count
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    available_ingredients = []
+    missing_ingredients = {}
+    ingredient_cocktail_count = {}
+    filter_ingredient = None
+
     if request.method == "POST":
-        ingredients_str = request.form["ingredients"]
-        ingredients = [ing.strip().lower() for ing in ingredients_str.split(",")]  # Split and clean
+        available_ingredients = request.form.getlist("available_ingredients")
+        missing_ingredients = request.form.getlist("missing_ingredients")
+        filter_ingredient = request.form.get("filter_ingredient")
 
-        suggested_cocktails, almost_suggested_cocktails = suggest_cocktails(ingredients)
+        # Move unchecked available ingredients to missing ingredients
+        available_ingredients = [ing for ing in available_ingredients if ing in request.form.getlist("available_ingredients")]
+        missing_ingredients = [ing for ing in missing_ingredients if ing not in request.form.getlist("missing_ingredients")]
 
-        return render_template("index.html", suggestions=suggested_cocktails, almost_suggestions=almost_suggested_cocktails)
-    else:
-        return render_template("index.html")
+        # Add checked missing ingredients to available ingredients
+        available_ingredients += [ing for ing in request.form.getlist("missing_ingredients") if ing not in available_ingredients]
+
+    suggested_cocktails, almost_suggested_cocktails, missing_ingredients, ingredient_cocktail_count = suggest_cocktails(available_ingredients)
+
+    if filter_ingredient:
+        suggested_cocktails = [(cocktail, ingredients) for cocktail, ingredients in suggested_cocktails if filter_ingredient in ingredients]
+        almost_suggested_cocktails = [(cocktail, missing) for cocktail, missing in almost_suggested_cocktails if filter_ingredient in cocktails[cocktail]]
+
+    return render_template("index.html", suggestions=suggested_cocktails, almost_suggestions=almost_suggested_cocktails, available_ingredients=available_ingredients, missing_ingredients=missing_ingredients, ingredient_cocktail_count=ingredient_cocktail_count, filter_ingredient=filter_ingredient)
 
 if __name__ == "__main__":
     app.run(debug=True) #REMOVE DEBUG=TRUE BEFORE DEPLOYMENT
